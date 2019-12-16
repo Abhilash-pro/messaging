aed64e1e62958fdf69627be199242d83605b6edd
Abhilash-pro <abhilash.3580.verma@gmail.com>
2019-12-16 17:52:48 +0530

    added network Connectivity indicator

--- messaging/App.js
+++ messaging/App.js
@@ -1,12 +1,55 @@
 import React from 'react';
-
+import {StyleSheet, View } from 'react-native';
+import Status from './components/status';
 
 export default class App extends React.Component {
 
+  renderMessageList(){
+    return (
+      <View style  = {styles.content}></View>
+    );
+  }
 
+  renderInputMethodEditor(){
+    return (
+      <View style = {styles.inputMethodEditor}></View>
+    );
+  }
+
+
+  renderToolBar(){
+    return (
+      <View style = {styles.toolbar}></View>
+    );
+  }
   render(){
     return (
-      <h1>New Messaging app</h1>
+      <View style ={styles.container}>
+      <Status/>
+        {this.renderMessageList()}
+        {this.renderToolBar()}
+        {this.renderInputMethodEditor()}
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  content:{
+    flex:1,
+    backgroundColor: 'white',
+  },
+  inputMethodEditor:{
+    flex:1,
+    backgroundColor: 'white',
+  },
+  toolbar:{
+    borderTopWidth: 1,
+    borderTopColor: 'rgba(0,0,0,0.04)',
+    backgroundColor:'white',
+  },
+});
